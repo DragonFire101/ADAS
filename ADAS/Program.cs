0ab13e74774a7474d4457a36f97c9e2a9ac30692
DragonFire101 <DragonFire101@users.noreply.github.com>
2021-01-19 07:12:39 -0500

    Add project files.

---
+++ ADAS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace ADAS
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+		{
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json")).Token;
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+
+     class Config
+    {
+        public string Token;
+    }
+    
+}
